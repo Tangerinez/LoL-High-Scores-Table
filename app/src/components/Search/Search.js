00d91a891a7735df2a5717113a1f33395ed60af1
Gregory Tang <gtang159@gmail.com>
2020-01-15 11:17:33 -0800

    Desktop responsive searchbar

--- app/src/components/Search/Search.js
+++ app/src/components/Search/Search.js
@@ -2,18 +2,38 @@ import React from "react";
 import "./Search.scss";
 import SearchIcon from "./SearchIcon/SearchIcon";
 
-function Search() {
-  return (
-    <div className="search-bar">
-      <img
-        src={require("../../images/logo-league.png")}
-        alt="search-bar-logo"
-        className="search-bar-league-logo"
-      />
-      <input className="input-field" placeholder="Search Summoner (NA)" />
-      <SearchIcon />
-    </div>
-  );
+class Search extends React.Component {
+  state = {
+    query: ""
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.query);
+  };
+
+  render() {
+    console.log(this.state.query);
+    return (
+      <div className="search-bar">
+        <img
+          src={require("../../images/logo-league.png")}
+          alt="search-bar-logo"
+          className="search-bar-league-logo"
+        />
+        <input
+          className="input-field"
+          onChange={this.handleChange}
+          placeholder="Search Summoner (NA)"
+        />
+        <SearchIcon handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
 }
 
 export default Search;
